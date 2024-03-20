b20734f982cab3b1a9284faf5b86cf547def3410
ked1125 <ked1125@naver.com>
2024-03-20 13:06:08 +0900

    240320_3rd_nodePractice

--- src/server.js
+++ src/server.js
@@ -8,12 +8,14 @@
 // });
 // app.listen(3000);
 // 기본세팅값들 end!!!
-const { User } = require("./models/User");
+// const { User } = require("./models/User");
 
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
 const dotenv = require("dotenv");
+
+const { userRouter } = require("./routes/userRouter");
 
 dotenv.config();
 
@@ -23,50 +25,71 @@ const server = async function () {
     console.log(" 디비 연결 완료~~");
     app.use(express.json());
 
-    app.get("/user", async function (req, res) {
-      try {
-        const users = await User.find();
-        return res.send({ user: users });
-      } catch (error) {
-        return res.status(500).send({ error: error.message });
-      }
-    });
-
-    app.get("/user/:userId", async function (req, res) {
-      try {
-        const { userId } = req.params;
-        const user = await User.findOne({ _id: userId });
-        return res.send({ user: user });
-      } catch (error) {
-        return res.status(500).send({ error: error.message });
-      }
-    });
-    // app.get("/user/:userId", async function (req, res) {
+    // app.get("/user", async function (req, res) {
     //   try {
+    //     const users = await User.find();
+    //     return res.send({ user: users });
     //   } catch (error) {
     //     return res.status(500).send({ error: error.message });
     //   }
     // });
 
-    app.post("/user", async function (req, res) {
-      try {
-        const user = new User(req.body);
-        await user.save();
-        return res.send(user);
-      } catch (error) {
-        return res.status(500).send({ error: error.message });
-      }
-    });
+    // app.get("/user/:userId", async function (req, res) {
+    //   try {
+    //     const { userId } = req.params;
+    //     const user = await User.findOne({ _id: userId });
+    //     return res.send({ user: user });
+    //   } catch (error) {
+    //     return res.status(500).send({ error: error.message });
+    //   }
+    // });
+    // // app.get("/user/:userId", async function (req, res) {
+    // //   try {
+    // //   } catch (error) {
+    // //     return res.status(500).send({ error: error.message });
+    // //   }
+    // // });
 
-    app.delete("/user/:userId", async function (req, res) {
-      try {
-        const { userId } = req.params.usereId;
-        const user = await User.findByIdAndDelete({ _id: userId });
-        return res.send({ user });
-      } catch (error) {
-        return res.status(500).send({ error: error.message });
-      }
-    });
+    // app.post("/user", async function (req, res) {
+    //   try {
+    //     const user = new User(req.body);
+    //     await user.save();
+    //     return res.send(user);
+    //   } catch (error) {
+    //     return res.status(500).send({ error: error.message });
+    //   }
+    // });
+
+    // app.delete("/user/:userId", async function (req, res) {
+    //   try {
+    //     const { userId } = req.params.usereId;
+    //     const user = await User.findByIdAndDelete({ _id: userId });
+    //     return res.send({ user });
+    //   } catch (error) {
+    //     return res.status(500).send({ error: error.message });
+    //   }
+    // });
+
+    // app.put("/user/:userId", async function (req, res) {
+    //   try {
+    //     const { age, email } = req.body;
+    //     const { userId } = req.params;
+    //     const user = await User.findByIdAndUpdate(
+    //       userId,
+    //       {
+    //         $set: { age, email },
+    //       },
+    //       {
+    //         new: true,
+    //       }
+    //     );
+    //     return res.send({ user });
+    //   } catch (error) {
+    //     return res.status(500).send({ error: error.message });
+    //   }
+    // });
+
+    app.use("/user", userRouter);
 
     app.listen(3000);
   } catch (error) {
